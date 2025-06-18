9cbbd4a81cf2f1df0cf8348e038ea5a131a51aaa
Abdullah Al Noman <Abdullah.Noman@dizishore.com>
2025-06-18 21:15:18 +0600

    added primitve

--- primitivetype/main.py
+++ primitivetype/main.py
@@ -1,6 +1,19 @@
 temperature = 23.7      
-humidity = 65            
+humidity = 65.12346           
 device_name = "TempSensor01"  
-is_online = True        
-print(temperature);
-print(humidity);
+is_online = True  
+      
+print(f"Temperature : {temperature}");
+print(f"Humidity: {humidity}");
+
+
+print(temperature,humidity,device_name,is_online,sep=" | ");
+print(temperature,humidity,device_name,is_online,sep=" , ");
+
+
+print("Temperature: "+str(temperature));
+print("Device: "+device_name);
+
+
+print("Device is : {}C".format(is_online));
+print("{:.1f}C {:.4f}".format(temperature,humidity))
