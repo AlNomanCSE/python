4a0f35c5177beeeb6f30eb6979e1ea52264f038c
Abdullah Al Noman <Abdullah.Noman@dizishore.com>
2025-06-23 20:06:17 +0600

    added panda

--- pandas_project/main.py
+++ pandas_project/main.py
@@ -1,16 +1,38 @@
 import pandas as pd  # type: ignore
-import numpy as np  # type: ignore
 
-new_2Darray = np.arange(0, 20).reshape(5, 4)
-print(new_2Darray)
-rowLable = [f"Row-{i+1}" for i in range(new_2Darray.shape[0])]
-columnLable = [f"Column*{i+1}" for i in range(new_2Darray.shape[1])]
-df = pd.DataFrame(data=new_2Darray, index=rowLable, columns=columnLable)
+df = pd.read_csv("test.csv")
 
-print(type(df))
-print(df.info())
-print(df.describe())
+print("🚨------🚨-------🚨-------🚨------🚨-----🚨-------🚨-----🚨")
+# print(df.shape)
+# print(df.info())
+# print(df.describe())
+# print(df.isnull().sum())
+print("🚨------🚨-------🚨-------🚨------🚨-----🚨-------🚨-----🚨")
 
-print(df.head())
-print(type(df[['Column*2','Column*1']]))
-print(type(df['Column*1']))
+# print(df[["Name", "Age", "Sex"]].head(10))
+print("🫵------🫵-------🫵-------🫵------🫵-----🫵-------🫵-----🫵")
+
+# sexFemale = df["Sex"] == "female"
+# print(len(df[sexFemale & (df["Pclass"] == 1)]))
+# print(df[(df["Age"] >= 20) & (df["Age"] <= 30)])
+# print(df[(df["Pclass"] == 1) | (df["Pclass"] == 2)])
+# print(df[(df["Age"] >= 18)][["Age","Name"]])
+# print(df['Pclass'].value_counts().sort_values())
+
+# print(df.groupby(["Sex","Pclass"]).size())
+# print(type(df.groupby("Sex")))
+# print(df.groupby("Sex")["Age"].mean())
+# print(df.groupby("Pclass")["Age"].mean())
+# print(df.groupby("Sex")["Age"].agg(["mean", "median", "std", "count"]))
+# print(df.groupby("Pclass")["Fare"].mean());
+# print(df.groupby("Embarked")["Fare"].agg(["mean", "median", "std", "count"]))
+
+# print(df.groupby(['Pclass','Sex'])["Fare"].mean().unstack())
+medians = df.groupby("Sex")["Age"].median()
+df["Age"] = df.apply(
+    lambda row: medians[row["Sex"]] if pd.isnull(row["Age"]) else row["Age"], axis=1
+)
+# print(df.isnull().sum())
+df_cleaned = df.copy()
+medians = df_cleaned.groupby("Sex")["Age"].median()
+print(df_cleaned.row)
